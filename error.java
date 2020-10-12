f03c29158118c720406f59e52178b0a7e7765436
unknown <2061007923@qq.com>
2020-10-12 12:37:29 +0800

    why?

---
+++ error.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class error {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		//构造一个Scanner对象(名为in)，现在我们可以用Scanner类的各种方法读取输入了
+		
+		//nextLine方法将读取一行输入(遇到'\n'停止)
+		System.out.println("What is your name?");
+		String name = in.nextLine();
+		System.out.println("Your name is: " + name + ".");
+		
+		//next方法可以读取一个单词(遇到分隔符，如空格，停止)
+		System.out.println("What is your mother name?");
+		String firstName = in.next();
+		System.out.println("Your mother firstname is: " + firstName + ".");
+
+	}
+
+}
