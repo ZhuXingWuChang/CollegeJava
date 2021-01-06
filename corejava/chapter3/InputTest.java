91f15fceab1e2e7742261328b87faa8a2b6093d1
ZhuXingWuChang <2061007923@qq.com>
2021-01-07 00:37:11 +0800

    Java核心技术卷I

---
+++ corejava/chapter3/InputTest.java
@@ -0,0 +1,25 @@
+package corejava.chapter3;
+
+import java.util.Scanner;
+
+/**
+ * This program demonstrates console input.
+ * Java核心技术3-2.
+ */
+
+public class InputTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // get first input
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+
+        // get second input
+        System.out.print("How old are you? ");
+        int age = in.nextInt();
+
+        // display output on console
+        System.out.println("Hello, " + name + ". Next year, you'll be " + (age + 1));
+    }
+}
