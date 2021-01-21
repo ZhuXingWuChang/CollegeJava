4b22c2a1d64e23a640921feeaa25b4c280619d74
ZhuXingWuChang <2061007923@qq.com>
2021-01-21 17:35:01 +0800

    Java核心技术卷I

--- corejava1/chapter5/resources/ResourceTest.java
+++ corejava1/chapter5/resources/ResourceTest.java
@@ -20,7 +20,7 @@ public class ResourceTest
         InputStream stream = cl.getResourceAsStream("data/about.txt");
         var about = new String(stream.readAllBytes(), "UTF-8");
         
-        InputStream stream2 = cl.getResourceAsStream("/corejava/title.txt");
+        InputStream stream2 = cl.getResourceAsStream("/corejava1/title.txt");
         var title = new String(stream2.readAllBytes(), StandardCharsets.UTF_8).trim();
         
         JOptionPane.showMessageDialog(null, about, title, JOptionPane.INFORMATION_MESSAGE, icon);
