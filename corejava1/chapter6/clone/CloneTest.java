0d5b9b4ce5c36944a5241abdbbbb4c492282f1dc
ZhuXingWuChang <2061007923@qq.com>
2021-02-01 21:27:28 +0800

    Java核心技术卷I

--- corejava1/chapter6/clone/CloneTest.java
+++ corejava1/chapter6/clone/CloneTest.java
@@ -10,7 +10,7 @@ public class CloneTest
     {
         var original = new Employee("John Q. Public", 50000);
         original.setHireDay(2000, 1, 1);
-        Employee copy = okriginal.clone();
+        Employee copy = original.clone();
         copy.raiseSalary(10);
         copy.setHireDay(2002, 12, 31);
         System.out.println("original=" + original);
