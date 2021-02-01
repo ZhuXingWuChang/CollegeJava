ad89850c1918a81d6099c376fd82613db1d91573
ZhuXingWuChang <2061007923@qq.com>
2021-02-01 21:26:58 +0800

    Java核心技术卷I

---
+++ corejava1/chapter6/clone/CloneTest.java
@@ -0,0 +1,19 @@
+package corejava1.chapter6.clone;
+
+/**
+ * This program demonstrates cloning.
+ * Java核心技术6-4.
+ */
+public class CloneTest
+{
+    public static void main(String[] args) throws CloneNotSupportedException
+    {
+        var original = new Employee("John Q. Public", 50000);
+        original.setHireDay(2000, 1, 1);
+        Employee copy = okriginal.clone();
+        copy.raiseSalary(10);
+        copy.setHireDay(2002, 12, 31);
+        System.out.println("original=" + original);
+        System.out.println("copy=" + copy);
+    }
+}
