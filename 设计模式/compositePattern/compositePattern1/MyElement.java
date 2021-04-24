0cadd1606fb33a83f7627f1dbc3cc3ca65e27c0f
诸行无常 <2061007923@qq.com>
2021-04-24 17:58:14 +0800

    Java设计模式

---
+++ 设计模式/compositePattern/compositePattern1/MyElement.java
@@ -0,0 +1,6 @@
+package 设计模式.compositePattern.compositePattern1;
+
+public abstract class MyElement
+{
+    public abstract void eat();
+}
---
+++ 设计模式/compositePattern/compositePattern1/Pear.java
@@ -0,0 +1,10 @@
+package 设计模式.compositePattern.compositePattern1;
+
+public class Pear extends MyElement
+{
+    @Override
+    public void eat()
+    {
+        System.out.println("吃梨子");
+    }
+}
